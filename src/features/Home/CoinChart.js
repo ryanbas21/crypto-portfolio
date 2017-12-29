922c4e228d2ba5976af0e03fcd6935a27a2851c5
Ryan Basmajian <ryanbas21@gmail.com>
2017-12-28 18:37:24 -0600

    tech(redux-logger): Added redux logger to the development tools
    Using redux logger to help debug code in development

--- src/features/Home/CoinChart.js
+++ src/features/Home/CoinChart.js
@@ -1,49 +1,49 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { map } from 'sanctuary';
-import { Table } from 'react-bootstrap';
+import {map} from 'sanctuary';
+import {Table} from 'react-bootstrap';
 
 const CoinChart = props => (
-  <div>
-    <Table hover responsive striped>
-      <thead>
-        <tr>
-          <th>Rank</th>
-          <th>Name</th>
-          <th>symbol</th>
-          <th>Price [USD]</th>
-          <th>24hr Volume [USD] </th>
-          <th>24hr Change</th>
-        </tr>
-      </thead>
-      <tbody>
-        {map(
+	<div>
+		<Table hover responsive striped>
+			<thead>
+				<tr>
+					<th>Rank</th>
+					<th>Name</th>
+					<th>symbol</th>
+					<th>Price [USD]</th>
+					<th>24hr Volume [USD] </th>
+					<th>24hr Change</th>
+				</tr>
+			</thead>
+			<tbody>
+				{map(
 					value => (
-  <tr key={value.name}>
-    <td>{value.rank}</td>
-    <td>{value.name}</td>
-    <td>{value.symbol}</td>
+						<tr key={value.name}>
+							<td>{value.rank}</td>
+							<td>{value.name}</td>
+							<td>{value.symbol}</td>
 
-    <td>{`$ ${value.price_usd.toLocaleString({ currency: 'usd' })}`} </td>
+							<td>{`$ ${value.price_usd.toLocaleString({currency: 'usd'})}`} </td>
 
-    <td>{`$ ${value['24h_volume_usd'].toLocaleString()}`} </td>
+							<td>{`$ ${value['24h_volume_usd'].toLocaleString()}`} </td>
 
-    <td
-      style={
+							<td
+								style={
 									value['value.percent_change_24h'] > 0
-										? { color: 'green' }
-										: { color: 'red' }
+										? {color: 'green'}
+										: {color: 'red'}
 								}
-    >
-      {`${value.percent_change_24h} %`}
-    </td>
-  </tr>
+							>
+								{`${value.percent_change_24h} %`}
+							</td>
+						</tr>
 					),
 					props.coins
 				)}
-      </tbody>
-    </Table>
-  </div>
+			</tbody>
+		</Table>
+	</div>
 );
 
 export default CoinChart;
--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,8 @@
-import { compose, applyMiddleware, createStore } from 'redux';
+import {compose, applyMiddleware, createStore} from 'redux';
 import createSagaMiddleware from 'redux-saga';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { routerMiddleware } from 'react-router-redux';
+import {logger} from 'redux-logger';
+import {composeWithDevTools} from 'redux-devtools-extension';
+import {routerMiddleware} from 'react-router-redux';
 import createHistory from 'history/createBrowserHistory';
 import reducers from '../reducers';
 import rootSaga from '../rootSaga';
@@ -10,7 +11,11 @@ const sagas = createSagaMiddleware();
 export const history = createHistory();
 
 const enhancer = compose(
-	applyMiddleware(sagas, routerMiddleware(history)),
+	applyMiddleware(
+		sagas,
+		routerMiddleware(history),
+		process.env.NODE_ENV === 'development' ? logger : f => f
+	),
 	typeof window !== 'undefined' && process.env.NODE_ENV !== 'production'
 		? window.devToolsExtension && window.devToolsExtension()
 		: f => f
