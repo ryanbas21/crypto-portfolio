d765ff0bf3f16572cadda66354dbcfe58f558004
Ryan Bas <18267769+ryanbas21@users.noreply.github.com>
2017-12-30 02:17:12 -0600

    Readme (#19)
    * tech(added-reducers): Added reducers to the combine reducers file
    
    Added the portfolio and navbar reducers to the combine reducers file
    
    * tech(waffle): Added waffle to readme
    
    Added waffle badge to the readme

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,12 @@
-import { combineReducers } from 'redux';
-import { routerReducer } from 'react-router-redux';
+import {combineReducers} from 'redux';
+import {routerReducer} from 'react-router-redux';
 import HomeReducer from '../features/Home/home.reducer';
+import NavbarReducer from '../features/Navbar/navbar.reducer';
+import PortfolioReducer from '../features/portfolio/portfolio.reducer';
 
 export default combineReducers({
 	Home: HomeReducer,
-	router: routerReducer
+	router: routerReducer,
+	navbar: NavbarReducer,
+	portfolio: PortfolioReducer
 });
