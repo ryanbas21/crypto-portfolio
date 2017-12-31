527a184ece885f9161c80a7eeb38b44f059ce0e7
Ryan Basmajian <ryanbas21@gmail.com>
2017-12-31 13:24:57 -0600

    bug(route-fix): Fixed home router double rendering on change

--- src/features/Home/api.js
+++ src/features/Home/api.js
@@ -1,8 +1,8 @@
-import axios from "axios";
-import {prop} from "sanctuary";
+import axios from 'axios';
+import {prop} from 'sanctuary';
 
 const {get} = axios;
 
 export default function coinMarketCapData() {
-	return get("https://api.coinmarketcap.com/v1/ticker/?limit=10").then(prop("data"));
+	return get('https://api.coinmarketcap.com/v1/ticker/?limit=10').then(prop('data'));
 }
