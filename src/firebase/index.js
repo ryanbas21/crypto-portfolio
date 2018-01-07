ea34bbdd4f2ae2bafb73278cd7e63e71cebaa51d
Ryan Bas <18267769+ryanbas21@users.noreply.github.com>
2018-01-07 17:16:51 -0600

    feat(firebase): Added firebaseo (#34)
    Added firebase for db

---
+++ src/firebase/index.js
@@ -0,0 +1,15 @@
+import firebase from 'firebase';
+import firebaseKeys from './firebase';
+
+function configureFirebase() {
+	try {
+		firebase.initializeApp(firebaseKeys.firebase);
+		console.log('initialized');
+	} catch (err) {
+		if (!/already exists/.test(err.message)) {
+			console.log(`Firebase initialization error => ${err}`);
+		}
+	}
+}
+
+export default configureFirebase;
